6ba03fccfbc7decdf55c2a11081061bddd5042b3
math-yew <math.u@hotmail.com>
2020-09-14 16:43:38 -0600

    revolve

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // totHeight = window.screen.availHeight;
 // totWidth = window.screen.availWidth;
 
+let previous = 0;
 $(window).on('scroll', function() {
   var winScroll = $(this).scrollTop();
 
+  let shifting = -Math.cos(winScroll/20);
+  let z = (previous > shifting) ? 10 : -10;
+  previous = shifting;
+
   $('.circle').css({
-    'transform': 'translate(-' + winScroll / 2 + '%)'
+    'transform': 'translate(' + -150 *shifting + '%,' + 200 * shifting + '%)',
+    'z-index': z
   });
 });
