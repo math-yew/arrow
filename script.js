8eb29eea75e0132b76c9e0192a9be7956a4413aa
math-yew <math.u@hotmail.com>
2020-09-16 11:15:37 -0600

    animation

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // totHeight = window.screen.availHeight;
 // totWidth = window.screen.availWidth;
 
+
+
+let docHeight = document.documentElement.scrollHeight;
 let previous = 0;
+console.log(document.documentElement.scrollTop);
+setTimeout(function () {
+  window.scrollTo(0, document.documentElement.scrollTop);
+},20);
 $(window).on('scroll', function() {
   var winScroll = $(this).scrollTop();
 
-  let shifting = -Math.cos(winScroll/20);
-  let z = (previous > shifting) ? 10 : -10;
+  let shifting = -Math.cos(winScroll/50);
+  let z = (previous > shifting) ? 1 : -1;
   previous = shifting;
 
   $('.circle').css({
-    'transform': 'translate(' + -150 *shifting + '%,' + 200 * shifting + '%)',
-    'z-index': z
+    'transform': 'translate(' + -150 *shifting + '%,' + 200 * shifting + '%) scale('+(1+.5*(1-Math.abs(shifting))*z)+')',
+    'z-index': z*10,
+
   });
+
+  if(winScroll > 100){
+    $('body').css({'background-color':'rgb('+255*winScroll/docHeight+',255,0)'});
+  }
+
+  // console.log($('.circle').first().position().top);
 });
